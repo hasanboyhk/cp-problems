00c2548637eb9737d6dc0502e44afabbf5143f37
Hasanboy Karimjonov <hasanboykarimjonovhk@gmail.com>
2026-02-22 07:54:27 +0500

    Integer20 completed

---
+++ qudrat/Integer/Integer15.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    cout << n / 10 % 10 << n / 100 << n % 10;
+    return 0;
+}
---
+++ qudrat/Integer/Integer19.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int s;
+    cin >> s;
+    cout << s / 60;
+    return 0;
+}
