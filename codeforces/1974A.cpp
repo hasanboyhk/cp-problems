cf848178b92be772402b2a14893cea352b01b3d9
Hasanboy Karimjonov <hasanboykarimjonovhk@gmail.com>
2026-02-22 20:47:53 +0500

    codeforces +2 22/2

---
+++ codeforces/1974A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x, y, cnt = 0, add;
+    cin >> x >> y;
+    cnt += (y % 2 == 0 ? y / 2 : y / 2 + 1);
+    add = (y % 2 == 0 ? 0 : 4);
+    cnt += (cnt * 7 + add >= x ? 0 : (int) ceil((x - cnt * 7 - add) / 15.0));
+    cout << cnt << "\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
