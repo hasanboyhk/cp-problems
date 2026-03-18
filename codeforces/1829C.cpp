3c81f9524aa9cfb9c62b9c1752eb499a34056d68
Hasanboy Karimjonov <hasanboykarimjonovhk@gmail.com>
2026-03-18 11:40:02 +0500

    clang-formatted

--- codeforces/1829C.cpp
+++ codeforces/1829C.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
+void solve() {
     int n, tmp, max1 = 1e9, max2 = 1e9, max3 = 1e9, result;
     string s;
     cin >> n;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> tmp >> s;
-        if(s == "01" && tmp < max1){
+        if (s == "01" && tmp < max1) {
             max1 = tmp;
-        }else if(s == "10" && tmp < max2){
+        } else if (s == "10" && tmp < max2) {
             max2 = tmp;
-        }else if(s == "11" && tmp < max3){
+        } else if (s == "11" && tmp < max3) {
             max3 = tmp;
         }
     }
@@ -19,12 +19,12 @@ void solve(){
     cout << (result == 1e9 ? -1 : result) << "\n";
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         solve();
     }
 }
--- codeforces/2071A.cpp
+++ codeforces/2071A.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
+void solve() {
     int n;
     cin >> n;
     cout << (n % 3 == 1 ? "YES\n" : "NO\n");
 }
 
-int main(){
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         solve();
     }
 }
